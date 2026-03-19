bacfcc1f94072ff47d72d23389d928f9396277c6
Pedropontocom <121833115+Pedropontocom@users.noreply.github.com>
2026-03-19 20:19:02 -0300

    Add files via upload

---
+++ fiaspo/ex11.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex11 {
+    public static void main(String[] args) {
+        boolean verdade = true;
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> produto = new ArrayList<>();
+        produto.add("agua");
+        produto.add("refrigerante");
+        produto.add("leite");
+        produto.add("cerveja");
+        produto.add("corote");
+
+        while (verdade) {
+            System.out.println("Pesquisa de produto: \nDigite 0 para sair:");
+            String valor = sc.nextLine();
+
+            if(valor.equals("0")){
+                verdade = false;
+            }else if(produto.contains(valor)){
+                System.out.println("Esse produto tem no estoque!");
+                System.out.println("");
+            }else{
+                System.out.println("Não tem esse produto no estoque!");
+                System.out.println("");
+            }
+        }
+        sc.close();
+    }
+}
